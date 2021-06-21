c794b6dd0dbf942ad23cfee6b572868e7fc0904d
Dingram91 <54293179+Dingram91@users.noreply.github.com>
2021-06-21 12:30:42 -0400

    updated profile page

---
+++ src/components/Logout.tsx
@@ -0,0 +1,45 @@
+import { ReactElement, useContext } from 'react'
+import {UserContext} from '../contexts/UserContext';
+import { useHistory } from 'react-router-dom';
+
+
+function Logout(): ReactElement {
+    const { user, setUser } = useContext(UserContext);
+    const history = useHistory();
+
+    const doLogout = () => {
+      fetch("http://localhost:3000/api/user/logout", {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            authToken: user!.token,
+          },
+          body: JSON.stringify({
+            authToken: user?.token
+          }),
+        }).then((response) => response.json())
+        .then((data) => {
+          // JSON data from response
+          console.log(JSON.stringify(data));
+  
+          if(!data.error) {
+            setUser!({token: ""});
+            console.log("Logout Success");
+            history.push("/");
+          } else {
+            alert(data.error);
+            return "Logout Failed"
+          }
+        })
+
+        return "Logout Success"
+    }
+
+    return (
+        <div>
+            {doLogout()}
+        </div>
+    )
+}
+
+export default Logout
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,123 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 
 type props = {
     user: IUser,
 }
 
 function Profile(props: props) {
+
+    const [profile, setProfile] = useState<ProfileInterface | undefined>(undefined);
+
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/profile", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            authToken: props.user.token,
+          },
+        }).then((response) => response.json())
+        .then((data) => {
+          // JSON data from response
+          console.log(JSON.stringify(data));
+  
+          if(!data.error) {
+            setProfile({
+                username: data.username,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                joined: data.joined,
+                thumbNail: data.thumbNail,
+                timeZone: data.timeZone,
+                invited: data.invited,
+                attending: data.attending,
+                defaultSchedule: data.defaultSchedule,
+            });
+          } else {
+            alert(data.error);
+          }
+  
+        }).catch((e) => {alert(e.message)});
+
+    }, [props.user.token]);
+
+    const TIMEZONES = [
+        "AST",
+        "EST",
+        "EDT",
+        "CST",
+        "CDT",
+        "MST",
+        "MDT",
+        "PST",
+        "AKDT",
+        "HST",
+        "HAST",
+        "HADT",
+        "SST",
+        "SDT",
+        "CHST",
+    ]
+
+    const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if(profile) setProfile({ ...profile, username: event.target.value });
+      };
+    const handleFirstNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if(profile) setProfile({ ...profile, firstName: event.target.value });
+      };
+      const handleLastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if(profile) setProfile({ ...profile, lastName: event.target.value });
+      };
+      const handleProfilePicChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if(profile) setProfile({ ...profile, thumbNail: event.target.value });
+      };
+      const handleTimeZoneChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        
+        // if(profile) setProfile({ ...profile, timeZone: event.target.value });
+      };
+
+    const inputStyle = "w-1/2 bg-pink-200 p-1 focus:bg-pink-300"
+    const inputStyleDisabled = "w-1/2 bg-pink-200 p-1"
+    const labelStyle = "w-1/2 bg-pink-200 p-1 focus"
+
     return (
         <div>
-            <h1>Welcome to your Profile!</h1>
-            <p>user is {(props.user) ? (props.user.username) : ("Blank")}</p>
-            <p>token is {(props.user) ? (props.user.token) : ("Blank")}</p>
+            <div className="flex flex-col w-1/2 bg-green-100">
+                <h1 className="col-span-2 self-center  pb-1">Welcome to your Profile!</h1>
+                <div className="flex border border-black pb-1">
+                    <label className={labelStyle}>username:</label>
+                    {/* <label className={inputStyle}>{profile?.username}</label> */}
+                    <input className={inputStyle} onChange={handleUsernameChange} type="text" value={profile? profile.username : ""} />
+                </div>
+                <div className="flex border border-black pb-1">
+                    <label className={labelStyle}>first name:</label>
+                    <input className={inputStyle} onChange={handleFirstNameChange} type="text" value={profile? profile.firstName : ""} />                </div>
+                <div className="flex border border-black pb-1">
+                    <label className={labelStyle}>last Name:</label>
+                    <input className={inputStyle} onChange={handleLastNameChange} type="text" value={profile? profile.lastName : ""} />                </div>
+                <div className="flex border border-black pb-1">
+                    <label className={labelStyle}>joined:</label>
+                    <input className={inputStyleDisabled} readOnly type="text" value={profile? profile.joined.toString() : ""} />                </div>
+                <div className="flex border border-black pb-1">
+                    <label className={labelStyle}>profile picture</label>
+                    <input className={inputStyle} onChange={handleProfilePicChange} type="text" value={profile? profile.thumbNail : ""} />                </div>
+                <div className="flex border border-black pb-1">
+                    <label className={labelStyle}>Time Zone:</label>
+                    {/* <input className={labelStyle} onChange={handleTimeZoneChange} type="text" value={profile? profile.timeZone : ""} />                </div> */}
+                    <select value={profile?.timeZone? profile.timeZone : "EST"}>
+                        {TIMEZONES.map(x => (<option value={x}>{x}</option>))}
+                    </select>
+                </div>
+                <div className="flex border border-black pb-1">
+                    <label className={labelStyle}>invited:</label>
+                    <input className={inputStyleDisabled} readOnly type="text" value={profile? profile.invited : ""} />                </div>
+                <div className="flex border border-black pb-1">
+                    <label className={labelStyle}>attending:</label>
+                    <input className={inputStyleDisabled} readOnly type="text" value={profile? profile.attending : ""} />                </div>
+                <div className="flex border border-black">
+                    <label className={labelStyle}>default schedule:</label>
+                    <input className={inputStyleDisabled} readOnly type="text" value={profile? profile.defaultSchedule.toString() : ""} />                </div>
+            </div>
         </div>
     )
 }
