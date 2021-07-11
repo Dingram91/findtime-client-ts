73d61b2b48b23983f89edad300d2e975c25dde64
Dingram91 <54293179+Dingram91@users.noreply.github.com>
2021-07-10 22:31:46 -0400

    removed tailwind css file

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -9,25 +9,6 @@ type UserContextType = {
   user: IUser | undefined;
   setUser: (user: IUser | undefined) => void;
 };
-
-enum TIMEZONE {
-  AST,
-  EST,
-  EDT,
-  CST,
-  CDT,
-  MST,
-  MDT,
-  PST,
-  AKDT,
-  HST,
-  HAST,
-  HADT,
-  SST,
-  SDT,
-  CHST,
-}
-
 interface ProfileInterface {
   username: string;
   firstName: string;
